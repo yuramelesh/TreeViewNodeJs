1bca0013d8a5949b617de186a6818aecbe495696
YuraMelesh <yuramelesh@gmail.com>
2016-03-15 12:05:15 +0200

    Work commit

--- data.js
+++ data.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
     user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
     password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
     port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
-    database : process.env.OPENSHIFT_APP_NAME
+    database : servernode
 });
 
 /**
